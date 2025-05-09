5873c420325f9142219c8b829a372e28fc5b6673
PavanKarthikGaraga <pavankarthik107@gmail.com>
2025-05-10 01:33:45 +0530

    chore: Schemas done

---
+++ backend/src/main/java/com/Voyage/repository/BookingRepository.java
@@ -0,0 +1,13 @@
+package com.Voyage.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Voyage.model.Booking;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByCustomerId(Long customerId);
+}
